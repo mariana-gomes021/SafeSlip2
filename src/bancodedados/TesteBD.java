dd1487bcfda75adbcb300476697e38ef231c9794
Luara Lima Oliveira <luaralima205@gmail.com>
2025-06-02 15:01:40 -0300

    feat: conexao com banco de dados adicionada + teste

---
+++ src/bancodedados/TesteBD.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bancodedados;
+
+import java.sql.*;
+
+/**
+ *
+ * @author luara
+ */
+public class TesteBD {
+
+    public static void main(String[] args) {
+        try (Connection conexao = ConexaoBD.getConexao()) {
+            if (conexao != null) {
+                System.out.println("Conexão bem-sucedida!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro na conexão: " + e.getMessage());
+        }
+    }
+}
